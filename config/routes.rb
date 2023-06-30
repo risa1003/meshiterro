0aaf8709353c8cb6140c17dcaff73cc1be61f150
EC2 Default User <ec2-user@ip-172-31-10-127.ap-northeast-1.compute.internal>
2023-06-30 12:58:51 +0000

    [Update]投稿画面の詳細と削除機能、ユーザーページの実装と編集機能の開始

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 end
